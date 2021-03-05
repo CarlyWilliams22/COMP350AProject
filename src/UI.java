6937b1b270e4c82746a5209464fe30fdb02fb8ed
MOODYMS18 <MOODYMS18@5CG829232X.GCC.edu>
2021-03-05 12:20:47 -0500

    Created a basic JavaFx window

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,9 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class UI {
+public class UI extends Application {
 
 	private FolderEngine fe;
 	private PlagiarismEngine algorithm;
@@ -8,8 +12,20 @@ public class UI {
 
 	}
 
-	public void render() {
+	@Override
+	public void start(Stage primary) throws Exception {
+		// TODO Auto-generated method stub
+		Group group = new Group();
 
+		Scene scene = new Scene(group, 800, 500); // width x height
+
+		primary.setTitle("Copied Code Catcher 2021");
+		primary.setScene(scene);
+		primary.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
