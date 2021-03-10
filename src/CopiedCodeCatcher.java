6ed62edcfc1cb5bad326f59d93e4f02cbc98751e
tirzahl <57695525+tirzahl@users.noreply.github.com>
2021-03-10 08:55:31 -0500

    working on testing the compare method

--- src/CopiedCodeCatcher.java
+++ src/CopiedCodeCatcher.java
@@ -14,6 +14,7 @@ public class CopiedCodeCatcher {
 //		File testFile = new File("FileStripTest.txt");
 //		pe.stripFile(testFile);
 		
+		System.out.println("Test student 1");
 		//create test student
 		Student testStud = new Student(1, "Tommy");
 		//give student a hardcoded txt file with dummy code
@@ -24,7 +25,28 @@ public class CopiedCodeCatcher {
 		pe.countKeywords(testStud);
 		//print the results
 		testStud.printKeywords();
+		
+		System.out.println("Test student 2");
+		//create test student
+		Student testStud2 = new Student(2, "Timmy");
+		//give student a hardcoded txt file with dummy code
+		File studentTestFile2 = new File("TimmyCode.txt");
+		testStud2.addFile(studentTestFile2);
+		pe.stripFile(testStud2);		
+		//count the keywords in the test student file
+		pe.countKeywords(testStud2);
+		//print the results
+		testStud2.printKeywords();
+		
+		pe.compare(testStud, testStud2);
+//		System.out.println("Score for tommy: " + testStud.getScore());
+//		
+//		System.out.println("Score for timmy: " + testStud2.getScore());
+		
+		for(int i = 0; i < testStud.getRedStudents().size(); i++) {
+			System.out.println("Students in red with tommy: " + testStud.getRedStudents().get(i));
 
+		}		
 	}
 
 }
